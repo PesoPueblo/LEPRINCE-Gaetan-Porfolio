26144e1707543c7ff22e7fb881f006570c181604
Gaëtan LEPRINCE <leprince.gaetan@yahoo.fr>
2023-06-27 17:49:59 +0200

    responsiv page competence

--- frontend/src/components/footer.jsx
+++ frontend/src/components/footer.jsx
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom"
 export default function Footer () {
     return (
         <div className="footer">
-            <h1>G.L</h1>
+            <h2>G.L</h2>
             <p>Gaëtan Lepince</p>
-            <div className="LinkRS">
+            <div className="LinkRS-footer">
                     <Link to="https://www.linkedin.com/in/gaetan-leprince-012601137/" className="linkres"><i className="fa-brands fa-linkedin"></i></Link>
                     <Link to="https://github.com/PesoPueblo" className="linkres"><i className="fa-brands fa-square-github"></i></Link>
             </div> 
