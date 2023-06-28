7aa6568be9d7588c89558a3dbb344d8e48153d35
Gaëtan LEPRINCE <leprince.gaetan@yahoo.fr>
2023-06-28 11:18:52 +0200

    correction texte bis

--- frontend/src/pages/apropos.jsx
+++ frontend/src/pages/apropos.jsx
@@ -7,7 +7,7 @@ export default function Apropos(){
                 J'ai, dans un premier temps, travaillé pendant plus de 10ans dans le commerce. Puis j'ai décidé de me lancer dans un domaine qui m'a toujours intéressé. <br />
                 Curieux de savoir ce qui ce passé derrière les écrans, j'ai commencé à apprendre le nécessaires pour savoir coder un site web basique! <br />
                 Curieux d'en apprendre toujours plus j'alterne entre pratique et auto-formation pour acquérir les bases des langages! <br />
-                Grâce à la plateforme OpenClassrooms je me suis lancé dans une formation à fin d'avoir un titre de niveau Bac+2 et je ne m'arrêterai pas là ... <br />
+                Grâce à la plateforme OpenClassrooms je me suis lancé dans une formation afin d'avoir un titre de niveau Bac+2 et je ne m'arrêterai pas là ... <br />
             </p>
             </div>
         </div>
