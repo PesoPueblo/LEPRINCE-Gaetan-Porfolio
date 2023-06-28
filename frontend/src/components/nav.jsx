e10d3f45bcdf3d16a1b28037cc1436b5ca9a9f7c
Gaëtan LEPRINCE <leprince.gaetan@yahoo.fr>
2023-06-28 11:34:15 +0200

    modif pour score lighthouse

--- frontend/src/components/nav.jsx
+++ frontend/src/components/nav.jsx
@@ -5,12 +5,12 @@ export default function Nav () {
     return (
         <div className="header">
             <img src={img} alt="Gaëtan LEPRINCE" />
-            <nav>
+            <menu>
                 <li><Link to="/" className="nav-link">A Propos</Link></li>
                 <li><Link to="/compétences" className="nav-link">Mes Compétences</Link></li>
                 <li><Link to="/projets"className="nav-link">Mes Projets</Link></li>
                 <li><Link to="/contact"className="nav-link">Me contacter</Link></li>
-            </nav>
+            </menu>
 
         </div>
     )
