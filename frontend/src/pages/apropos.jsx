b77a0e4bc8bf0c00ef75ab591e69ac10658ac1dd
Gaëtan LEPRINCE <leprince.gaetan@yahoo.fr>
2023-06-28 11:13:15 +0200

    correction texte

--- frontend/src/pages/apropos.jsx
+++ frontend/src/pages/apropos.jsx
@@ -4,9 +4,8 @@ export default function Apropos(){
             <h2 className="title">Qui suis-je ?</h2>
             <div>
             <p className="Text">
-                Ne partons pas sur un débats philosphique sur la condition humaine ... <br />
-                Après avoir travaillé pendant plus de 10ans dans le commerce j'ai décidé de me lancer dans le monde de l'IT. <br />
-                Toujours curieux de ce qui ce passé derrière le visuel, j'ai commencé à apprendre les langagues nécessaires pour savoir coder un site web! <br />
+                J'ai, dans un premier temps, travaillé pendant plus de 10ans dans le commerce. Puis j'ai décidé de me lancer dans un domaine qui m'a toujours intéressé. <br />
+                Curieux de savoir ce qui ce passé derrière les écrans, j'ai commencé à apprendre le nécessaires pour savoir coder un site web basique! <br />
                 Curieux d'en apprendre toujours plus j'alterne entre pratique et auto-formation pour acquérir les bases des langages! <br />
                 Grâce à la plateforme OpenClassrooms je me suis lancé dans une formation à fin d'avoir un titre de niveau Bac+2 et je ne m'arrêterai pas là ... <br />
             </p>
