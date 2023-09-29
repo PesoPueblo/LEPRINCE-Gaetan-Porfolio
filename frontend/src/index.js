0e0a5ae67e499b534c492b49ee5d50112f705db5
Gaëtan LEPRINCE <leprince.gaetan@yahoo.fr>
2023-09-29 13:23:42 +0200

    annulation modif routes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,28 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Presentation from './pages/presentation';
-import Details from './pages/details'
-import Competences from './pages/competences';
-import Works from './pages/projets';
-import Contact from './pages/contact';
-import img from "./asset/moi.jpg";
-import { Route} from 'react-router-dom';
+import App from './pages/App';
+import Nav from './components/nav';
+import Footer from './components/footer';
 import {BrowserRouter as Router} from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
-      <Route path='/' element={<Presentation
-              titre='Bienvenue !!!'
-              text='Bonjour, je suis Gaëtan, Développeur Web Front-end Junior'
-              img={img}/>
-        }></Route>
-        <Route exact path='/compétences'element={<Competences/>}></Route>
-        <Route exact path='/projets' element={<Works/>}></Route>
-        <Route path='/projets/:titre' element={<Details/>}></Route> 
-        <Route path='/contact' element={<Contact/>}></Route>
+      <Nav/>
+      <App />
+      <Footer/>
     </Router>
   </React.StrictMode>
 );
